7ecf8b80c9dafa0fea5d178c46dbf17360df0895
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-03-01 17:10:23 +0300

    Add validation attributes for Email and Password properties in User class

--- StreamService.Entities/Concrete/User.cs
+++ StreamService.Entities/Concrete/User.cs
@@ -16,7 +16,15 @@ public class User : BaseEntity
     public string LastName { get; set; } = default!;
 
     [EmailAddress]
+    [Required]
     public string Email { get; set; } = default!;
+
+    [Required]
+    [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 20 characters.")]
+    [RegularExpression(
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    )]
     public string Password { get; set; } = default!;
     public Membership Membership { get; set; }
 
